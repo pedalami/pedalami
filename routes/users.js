31e022f9b698505b521e7a85e2296012216a16f5
Combii <davidstovle@gmail.com>
2021-11-05 14:31:52 +0100

    Working update points (But replacing)

--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,64 @@ var express = require('express');
 var app = express.Router();
 const mongoose = require('mongoose');
 
-
 // Schema
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    token: String,
-    email: String
-})
+  token: String,
+  email: String,
+  points: Number,
+});
 
 // Model
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model('User', UserSchema);
 
 app.get('/create', (req, res) => {
-    console.log("Received createUser GET request:")
-    console.log(req.body)
-    if(req.body.token && req.body.email) {
-    const newUser = new User(req.body)
+  console.log('Received createUser GET request:');
+  console.log(req.body);
+  if (req.body.token && req.body.email) {
+    const newUser = new User(req.body);
+    newUser.points = 0;
+
     newUser.save((error) => {
-        if(error) {
-            console.log('Error saving the user.');
-            res.send('Error saving the user!');
-        } else {
-            console.log('The user has been saved.')
-            res.send('User saved correctly!');
-        }
-    })
-    } else {
-        console.log('Error: Missing parameters.')
-        res.send('Error: Missing parameters.');
+      if (error) {
+        console.log('Error saving the user.');
+        res.send('Error saving the user!');
+      } else {
+        console.log('The user has been saved.');
+        res.send('User saved correctly!');
+      }
+    });
+  } else {
+    console.log('Error: Missing parameters.');
+    res.send('Error: Missing parameters.');
+  }
+});
+
+app.get('/addPoints', (req, res) => {
+  //Read token
+  //Number of points to add for ID Profile
+
+  var query = { token: req.body.token };
+  
+  
+  var newUpdate = {
+      points: req.body.points
+  }
+
+  User.findOneAndUpdate(
+    query,
+    newUpdate,
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return res.send('Succesfully saved.');
     }
-})
+  );
+});
+
+app.patch('/removePoints', (req, res) => {
+  //Read Token
+  //Number of points to remove for ID Profile
+});
 
 module.exports = app;
