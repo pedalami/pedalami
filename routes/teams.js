80c1534de16cb46a8fc80ba75043f0e64f26191e
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2021-11-23 14:26:48 +0100

    Update teams.js

--- routes/teams.js
+++ routes/teams.js
@@ -47,6 +47,7 @@ app.get('/search', (req, res) => {
   console.log(req.body);
   const to_search = req.query.name;
   if (to_search) {
+    console.log(to_search);
     const teams = Team.find({ name: "test" }, (error, team) => {
       if (error) {
         console.log('Error finding the user.');
