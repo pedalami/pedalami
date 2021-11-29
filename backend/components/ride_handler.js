20642bc5d5e5830575bb0706e6c5ad801f81a7ac
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2021-11-29 20:34:37 +0100

    Added 1st version of gamification controller

--- backend/components/ride_handler.js
+++ backend/components/ride_handler.js
@@ -5,19 +5,20 @@ const mongoose = require('mongoose');
 const Double = mongoose.Schema.Types.Number;
 const Schema = mongoose.Schema;
 const UserSchema = Schema.UserSchema;
+const gamification_controller = require('./gamification_controller.js');
 
 
 // Schema
 const RideSchema = new Schema({
     user_uid: { type: String, required: true },
     name: { type: String, required: true },
-    duration_in_seconds: { type: Double, required: true }, 
-    total_km: { type: Double, required: true }, 
+    duration_in_seconds: { type: Double, required: true },
+    total_km: { type: Double, required: true },
     pace: { type: Double, required: true }, //Average speed in km/h
     date: { type: Date, required: true },
     elevation_gain: { type: Double, required: true },
-    points: { type: Double, required: false},
-  });
+    points: { type: Double, required: false },
+});
 
 // Model
 const User = mongoose.model('User', UserSchema);
@@ -32,23 +33,24 @@ app.post('/record', (req, res) => {
     ride.name = req.body.name
     ride.duration_in_seconds = req.body.duration_in_seconds
     ride.total_km = req.body.total_km
-    ride.pace =  req.body.total_km/(req.body.duration_in_seconds/3600)
+    ride.pace = req.body.total_km / (req.body.duration_in_seconds / 3600)
     ride.date = req.body.date
     ride.elevation_gain = req.body.elevation_gain
     // We cannot do User.findById since the uid is not the _id
-    if (req.body.user_uid && User.findOne({ uid: req.body.user_uid })){
-    ride.save().then(() => {
-        //chiama gamification controller
-        res.json({
-            'message': 'Ride saved successfully',
-            'points': ride.points,
-            'pace' : ride.pace,
-            'id' : ride._id
+    if (req.body.user_uid && User.findOne({ uid: req.body.user_uid })) {
+        Promise.all([
+            gamification_controller.assign_points(ride)
+        ]).then(() => {
+            res.json({
+                'message': 'Ride saved successfully',
+                'points': ride.points,
+                'pace': ride.pace,
+                'id': ride._id
             });
-    }).catch((err) => {
-        console.log(err);
-        res.sendStatus(500).send("Cannot save the ride in the DB");
-    });
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500).send("Cannot save the ride in the DB");
+        });
     }
     else {
         console.log('Cannot find the user specified!\n');
@@ -60,20 +62,20 @@ app.post('/record', (req, res) => {
 app.get('/getAllByUserId', (req, res) => {
     console.log('Received getAllByUserId GET request:');
     console.log("User:", req.query.user_uid);
-    
-    if(req.query.user_uid){
+
+    if (req.query.user_uid) {
         // I return an array of rides without the fields _id and __v
-        Ride.find({ user_uid: req.query.user_uid },'-_id -__v', (error, rides) => {
+        Ride.find({ user_uid: req.query.user_uid }, '-_id -__v', (error, rides) => {
             if (error) {
-            console.log('Error finding the rides of the specified user_uid.\n'+error);
-            res.status(500).send('Error finding the rides!');
+                console.log('Error finding the rides of the specified user_uid.\n' + error);
+                res.status(500).send('Error finding the rides!');
             } else {
-            res.status(200).send(rides);
+                res.status(200).send(rides);
             }
         });
     } else {
-      console.log('Error: Missing the user_uid parameter.');
-      res.status(400).send('Error: Missing the user_uid parameter.');
+        console.log('Error: Missing the user_uid parameter.');
+        res.status(400).send('Error: Missing the user_uid parameter.');
     }
 });
 
