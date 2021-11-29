405bddecd733dcbe6da391078e549cffc635433f
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2021-11-29 21:14:02 +0100

    Added dart interface for recordRide

--- lib/services/mongodb_service.dart
+++ lib/services/mongodb_service.dart
@@ -63,4 +63,34 @@ class MongoDB {
     return response.statusCode == 200 ? true : false;
   }
 
+  //Returns the recorded ride if everything went fine
+  //Returns null in case of error
+  Future<List<String>?> recordRide(String userID, String name, int durationInSeconds, double total_km, Date date, double elevation_gain) async {
+    var url = Uri.parse('https://pedalami.herokuapp.com/rides/record')
+    var response = await _serverClient.post(url,
+        headers: _headers,
+        body: json.encode({
+          "uid": userID,
+          "name": name,
+          "duration_in_seconds": durationInSeconds,
+          "total_km": total_km,
+          "date": date.toString(),
+          "elevation_gain": elevation_gain})
+          );
+    if (response.statusCode == 200) {
+      var decodedBody = json.decode(response.body) as List;
+      return decodedBody; //here, after implementing the ride class, we can return the ride object updated with the values in the serponse
+      /*
+      The response body has this structure:
+      {
+        "message": "Ride saved successfully",
+        "points": 10100,
+        "pace": 7200,
+        "id": "61a53224c7a4d074a77aa6bc"
+      }
+      */
+    } else
+      return null;
+  }
+
 }
