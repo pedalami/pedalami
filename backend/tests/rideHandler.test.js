715f7e2d188353013e4b82e27e25f1fa6b674110
Ita97 <lorenzo.ita@hotmail.com>
2021-12-12 11:18:15 +0100

    rideHandler.test.js: increased timeout

--- backend/tests/rideHandler.test.js
+++ backend/tests/rideHandler.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./../../server');
 const mongoose = require('mongoose');
 
-jest.setTimeout(10000);
+jest.setTimeout(15000);
 
 beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
