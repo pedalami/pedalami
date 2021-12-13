8a77746177a8d9c1250777dd58a30a68356ed449
MaxGio98 <48352868+MaxGio98@users.noreply.github.com>
2021-12-14 00:23:47 +0100

    UI update

--- lib/routes/rewards_page.dart
+++ lib/routes/rewards_page.dart
@@ -17,63 +17,31 @@ class RewardPage extends StatefulWidget {
   _RewardPageState createState() => _RewardPageState();
 }
 
-class _RewardPageState extends State<RewardPage> with SingleTickerProviderStateMixin {
-
-  List<Reward> rewards=[];
+class _RewardPageState extends State<RewardPage> {
+  List<Reward> rewards = [];
   late bool loading;
-  late ScrollController _scrollController;
-  late AnimationController _hideFabAnimController;
 
   refresh() {
     setState(() {});
   }
-  void loadScrollController() {
-    _scrollController = ScrollController();
-    _hideFabAnimController = AnimationController(
-      vsync: this,
-      duration: kThemeAnimationDuration,
-      value: 1, // initially visible
-    );
 
-    _scrollController.addListener(() {
-      switch (_scrollController.position.userScrollDirection) {
-      // Scrolling up - forward the animation (value goes to 1)
-        case ScrollDirection.forward:
-          _hideFabAnimController.forward();
-          break;
-      // Scrolling down - reverse the animation (value goes to 0)
-        case ScrollDirection.reverse:
-          _hideFabAnimController.reverse();
-          break;
-      // Idle - keep FAB visibility unchanged
-        case ScrollDirection.idle:
-          break;
-      }
-    });
-  }
   @override
   void initState() {
-    loading=true;
-    loadScrollController();
-    _scrollController.addListener(() => setState(() {}));
+    loading = true;
     WidgetsBinding.instance!.addPostFrameCallback((timeStamp) async {
-      rewards=(await MongoDB.instance.getRewards())!;
-      loading=false;
-      setState(() {
-
-      });
+      rewards = (await MongoDB.instance.getRewards())!;
+      loading = false;
+      setState(() {});
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    String points= LoggedUser.instance!.points!.toStringAsFixed(0);
+    String points = LoggedUser.instance!.points!.toStringAsFixed(0);
     return Scaffold(
-      floatingActionButton: FadeTransition(
-        opacity: _hideFabAnimController,
-        child: ScaleTransition(
-          scale: _hideFabAnimController,
+        floatingActionButton: Container(
+          height: 50,
           child: FloatingActionButton.extended(
             backgroundColor: Colors.green[600],
             heroTag: "btnShowRedeemedRewards",
@@ -84,34 +52,44 @@ class _RewardPageState extends State<RewardPage> with SingleTickerProviderStateM
             icon: Icon(Icons.list),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        child: Column(
-          children: [
-            Container(
-              color: Colors.green[600],
-              height: 20 * SizeConfig.heightMultiplier!,
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    top: 3 * SizeConfig.heightMultiplier!),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("Rewards", style: TextStyle(color: Colors.white, fontSize: 40),),
-                    Text("You currently have "+points+(points=="1"?" point":" points"), style: TextStyle(color: Colors.white, fontSize: 15),),
-
-                  ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.green[600],
+                height: 20 * SizeConfig.heightMultiplier!,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(top: 3 * SizeConfig.heightMultiplier!),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Rewards",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                      Text(
+                        "You currently have " +
+                            points +
+                            (points == "1" ? " point" : " points"),
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            loading?Text("Loading..."):(rewards.length==0?Text("No rewards available"):
-            RewardItem(rewards: rewards,notifyParent: refresh))
-          ],
-        ),
-      )
-    );
+              loading
+                  ? Text("Loading...")
+                  : (rewards.length == 0
+                      ? Text("No rewards available")
+                      : RewardItem(rewards: rewards, notifyParent: refresh)),
+              SizedBox(
+                height: 50,
+              )
+            ],
+          ),
+        ));
   }
 }
