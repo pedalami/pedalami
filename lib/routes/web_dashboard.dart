6f38b51d192f6ca4d73ad6bab9f13759cf615e1c
MarcusM94 <marcus.mortstrand@outlook.com>
2021-12-19 16:51:52 +0100

    Some updates to web dashboard

--- lib/routes/web_dashboard.dart
+++ lib/routes/web_dashboard.dart
@@ -1,22 +1,102 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pedala_mi/models/loggedUser.dart';
+import 'package:pedala_mi/services/mongodb_service.dart';
 
 class WebDashBoard extends StatefulWidget {
-  const WebDashBoard({Key? key}) : super(key: key);
+  final context;
+
+  const WebDashBoard({Key? key, this.context}) : super(key: key);
 
   @override
   _WebDashBoardState createState() => _WebDashBoardState();
 }
 
 class _WebDashBoardState extends State<WebDashBoard> {
+  LoggedUser? _miUser = LoggedUser.instance;
+  MongoDB db = MongoDB();
+
+  Widget showStats() {
+    return _miUser == null
+        ? Container(
+            height: MediaQuery.of(context).size.height / 5,
+            width: MediaQuery.of(context).size.width / 8,
+            child: CircularProgressIndicator(
+              strokeWidth: 6.0,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+              backgroundColor: Colors.grey,
+            ),
+          )
+        : Row(
+            children: [
+              Column(
+                children: [
+                  Text("HEJ"),
+                ],
+              )
+            ],
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HEJ"),
+        toolbarHeight: MediaQuery.of(context).size.width / 20,
+        leadingWidth: MediaQuery.of(context).size.width / 8,
+        leading: Transform.scale(
+          scale: 3,
+          child: Padding(
+            padding: EdgeInsets.only(top: 5.0),
+            child: Image.asset(
+              'lib/assets/pedala_logo.png',
+              height: MediaQuery.of(context).size.height / 8,
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(
+              right: MediaQuery.of(context).size.width / 90,
+              top: MediaQuery.of(context).size.height / 90,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("Marcus"),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 90,
+                    ),
+                    FaIcon(FontAwesomeIcons.user)
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 90),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Logout",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 90,
+                      ),
+                      FaIcon(
+                        FontAwesomeIcons.signOutAlt,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+        backgroundColor: Colors.green.shade200,
       ),
-      body: Text("HEJ"),
+      body: Center(child: showStats()),
     );
   }
 }
