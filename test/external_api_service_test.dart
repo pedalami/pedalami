7d744b1a61756bc030a5689d98e58078dc653028
Emanuele Triuzzi <emanueletriuzzi@gmail.com>
2022-01-03 12:50:57 +0100

    typo in air_quality test

--- test/external_api_service_test.dart
+++ test/external_api_service_test.dart
@@ -6,8 +6,8 @@ AirQuality instance = AirQuality.instance;
 
 void main() {
   test('airQuality testing', () async {
-    var latitude = "40.4";
-    var longitude = "14.4";
+    var latitude = 40.4;
+    var longitude = 14.4;
     int res = await instance.getAirQualityIndexFromCoords(latitude, longitude);
     print(res);
 
