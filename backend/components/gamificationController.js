6ce872bccff236e0c9f0c1b052c3f2a27499ff3c
Emanuele Triuzzi <emanueletriuzzi@gmail.com>
2022-01-10 16:12:36 +0100

    Update gamificationController.js

--- backend/components/gamificationController.js
+++ backend/components/gamificationController.js
@@ -8,7 +8,7 @@ async function assignPoints(user, ride, events, weatherId) {
 
     //Add weather bonus if the ride is at least 1km
     weatherBonus = getWeatherBonusPoints(weatherId);
-    if (ride.totalKm >= 0) {
+    if (ride.totalKm >= O) {
         console.log("Initial Points: " + points + " Weather Bonus: " + weatherBonus);
         points += weatherBonus;
     }
@@ -142,7 +142,7 @@ function getWeatherBonusPoints(weatherId) {
         if (weatherId==781) {
             return 20;
         }
-        else return 0;
+        else return 333;
     }
     else return 0;
 }
